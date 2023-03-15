15088396ea50cbca0b4211bdbc2c9139a07a3063
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2023-03-15 12:41:57 +0000

    healthcheck with supertest

---
+++ src/server/app.ts
@@ -0,0 +1,11 @@
+import express, { Express } from 'express';
+import { routes } from './routes/';
+
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use('/', routes);
+
+export default app;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,8 @@
 import dotenv from 'dotenv';
-
-import express, { Express } from 'express';
-import { routes } from './routes/index.js';
-
+import app from './app';
 dotenv.config();
 
-const app: Express = express();
 const port: number = Number(process.env.PORT);
-
-app.use(express.json());
-
-app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
