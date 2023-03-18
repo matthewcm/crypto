8770f15603fb9b3cdba83bcc8f62bfc377b66d19
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2023-03-18 02:34:23 +0000

    middleware

---
+++ server/src/middleware/auth-middleware.ts
@@ -0,0 +1,9 @@
+import { auth } from 'express-oauth2-jwt-bearer';
+
+const jwtCheck = auth({
+	audience: 'http://localhost:5000',
+	issuerBaseURL: 'https://dev-d14rcn3tzx7dpm33.uk.auth0.com/',
+	tokenSigningAlg: 'RS256',
+});
+
+export default jwtCheck;
