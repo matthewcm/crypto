a15866a77bec1c48c674b95c17d6324f0824ff92
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2023-03-18 16:19:07 +0000

    tests: market-summary

--- server/src/routes/market-summary.ts
+++ server/src/routes/market-summary.ts
@@ -2,24 +2,19 @@
 import axios from 'axios';
 import { type Request, type Response, Router, RequestHandler, NextFunction } from 'express';
 import jwtCheck from '../middleware/auth-middleware';
+import { MarketSummary } from '../types/MarketSummary';
 
 export const marketSummary = Router();
 
-export interface BittrexMarketSummary {
-  symbol: string;
-  high: number;
-  low: number;
-  volume: number;
-  quoteVolume: number;
-  percentChange: number;
-  updatedAt: string;
-}
+
 
 marketSummary.get('/summary', jwtCheck,  (async (request: Request, response: Response, next: NextFunction)=>{
   try {
     const res = await axios.get('https://api.bittrex.com/v3/markets/summaries');
 
-    const marketSummaryData = res.data as BittrexMarketSummary[];
+    const marketSummaryData = res.data as MarketSummary[];
+
+    console.log('marketSummaryData', marketSummaryData);
 
     console.log('res', res);
     response.status(200).json(marketSummaryData).end();
