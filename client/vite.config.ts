31a571190a9a3e3c05845e1fa296461511e3bc77
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2023-03-18 14:57:51 +0000

    default env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,18 +5,18 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		svgr(),
-		react(),
-		EnvironmentPlugin(['AUTH0_DOMAIN', 'AUTH0_CLIENT_ID', 'AUTH0_AUDIENCE']),
-	],
-	server: {
-		host: true,
-		port: 8000,
-	},
-	define: {
-		'process.env.AUTH0_DOMAIN': JSON.stringify('production'),
-		'process.env.AUTH0_CLIENT_ID': JSON.stringify('production'),
-		'process.env.AUTH0_AUDIENCE': JSON.stringify('production'),
-	},
+  plugins: [
+    svgr(),
+    react(),
+    EnvironmentPlugin({
+
+      AUTH0_DOMAIN: '',
+      AUTH0_CLIENT_ID:'',
+      AUTH0_AUDIENCE:'',
+    }),
+  ],
+  server: {
+    host: true,
+    port: 8000,
+  },
 });
