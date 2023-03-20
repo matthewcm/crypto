85b18bdea6ee1688c0f140032c25674dbb43ee38
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2023-03-20 19:37:23 +0000

    use market symbol and only show relevant decimal places

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -2,6 +2,7 @@
 import { percentChangeColor } from '../../utils/percentColor';
 import { MarketSummary } from '../../types/MarketSummary';
 import PaginationRow from './PaginationRow';
+import MarketSymbol from '../MarketSymbol/MarketSymbol';
 import { usePagination } from './usePagination';
 
 interface TableProps {
@@ -32,11 +33,11 @@ const Table = ({ data }: TableProps ) => {
       <table className="table w-full">
         <thead>
           <tr>
-            <th>Market</th>
+            <th>Market: Base-Quote</th>
             <th className='hidden sm:table-cell'>Low</th>
             <th className='hidden sm:table-cell'>High</th>
-            <th className='hidden lg:table-cell'>Quote Volume</th>
-            <th className='hidden lg:table-cell'>Volume</th>
+            <th className='hidden lg:table-cell'>24h Base Volume</th>
+            <th className='hidden lg:table-cell'>24h Quote Volume</th>
             <th>24h Change</th>
           </tr>
         </thead>
@@ -44,11 +45,11 @@ const Table = ({ data }: TableProps ) => {
 
           {page.map((market) => (
             <tr key={market.symbol}>
-              <td>{market.symbol}</td>
-              <td className="hidden sm:table-cell">{market.low}</td>
-              <td className="hidden sm:table-cell">{market.high}</td>
-              <td className="hidden lg:table-cell">{market.quoteVolume}</td>
-              <td className="hidden lg:table-cell">{market.volume}</td>
+              <td><MarketSymbol symbol={market.symbol}/></td>
+              <td className="hidden sm:table-cell">{((Math.round((market.low) * 100000000) / 100000000).toFixed(8))}</td>
+              <td className="hidden sm:table-cell">{((Math.round((market.high) * 100000000) / 100000000).toFixed(8))}</td>
+              <td className="hidden lg:table-cell">${((Math.round((market.volume) * 100) / 100).toFixed(2))}</td>
+              <td className="hidden lg:table-cell">${((Math.round((market.quoteVolume) * 100) / 100).toFixed(2))}</td>
               <td className={`${percentChangeColor(Number(market.percentChange))}`}>{Number(market.percentChange || 0)}% </td>
             </tr>
           ))}
